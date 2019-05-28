bc18f7f851cfb0ed35dee9d4b4ef9d272fef3e27
Shuhao <rzsh2003@gmail.com>
2019-05-28 21:42:14 +0100

    instruction

--- codegen/instr.hs
+++ codegen/instr.hs
@@ -8,10 +8,9 @@ data Instr where
     CInitChan :: Channel a -> Instr
     CDeleteChan :: Channel a -> Instr
     CSend :: Channel a -> Exp a -> Instr
-    CRecv :: Channel a -> Exp a -> Instr
+    CRecv :: Channel a -> Int -> Instr
     CEnd :: Exp a -> Instr
     CDecla :: Int -> ReprType a -> Instr
     CAssgn :: Int -> Exp a -> Instr
-    CBranch :: Exp a -> Seq Instr -> Seq Instr -> Instr
-    CSelect :: Int -> Int -> Int -> Seq Instr -> Seq Instr -> Instr
-    CEither2Label :: Int -> Int -> Instr
+    CBranch :: Int -> Seq Instr -> Seq Instr -> Instr
+    CSelect :: Int -> Int -> Seq Instr -> Seq Instr -> Instr
