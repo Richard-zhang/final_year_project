26357a299ad9ccac0b2da95173c2496e86de9c47
Shuhao <rzsh2003@gmail.com>
2019-05-29 22:28:21 +0100

    code gen from instr to C

---
+++ codegen/monad.hs
@@ -0,0 +1,10 @@
+data CodeGenState = CodeGenState
+  {
+    chanTable :: Map ChanKey CID,
+    varNext   :: Int,
+    chanNext  :: CID,
+    dataStructCollect :: Set AReprType
+  }
+
+newtype CodeGen m a = CodeGen { runCodeGen :: StateT CodeGenState m a }
+    deriving (Functor, Applicative, Monad, MonadState CodeGenState, MonadIO)
