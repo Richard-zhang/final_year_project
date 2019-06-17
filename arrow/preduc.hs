734bdedf52f73fee545603a82ebef0aafd42d8ab
Shuhao <rzsh2003@gmail.com>
2019-06-17 03:29:14 +0100

    arrow continued

--- arrow/preduc.hs
+++ arrow/preduc.hs
@@ -1,4 +1,4 @@
 preduc :: SArrow (a, a) a -> SArrow (a, (a, (a, a))) a
-preduc r = helper >>> (r *** r) >>> r
+preduc r = assoc >>> (r *** r) >>> r
  where
-  helper = (arr Id *** arr Fst) &&& (arr Snd >>> arr Snd)
+  assoc = (arr Id *** arr Fst) &&& (arr Snd >>> arr Snd)
