44cd481d141fc4e3c0350c31b66d900f4e49e41e
Shuhao <rzsh2003@gmail.com>
2019-06-18 13:21:14 +0100

    margin

--- background/fm-helper.hs
+++ background/fm-helper.hs
@@ -1,7 +1,10 @@
 liftF :: Functor f => f a -> Free f a
 liftF = Free . fmap Pure
 
-freeM :: (Functor f, Functor g) => (f a -> g a) -> (Free f a) -> (Free g a)
+freeM :: (Functor f, Functor g) 
+    => (f a -> g a) 
+    -> (Free f a) 
+    -> (Free g a)
 freeM phi (Pure x ) = Pure x
 freeM phi (Free fa) = Free $ phi (fmap (freeM phi) fa)
 
@@ -9,5 +12,7 @@ monad :: Monad m => Free m a -> m a
 monad (Pure x  ) = pure x
 monad (Free mfx) = mfx >>= monad
 
-interpret :: (Functor f, Monad m) => (f a -> m a) -> (Free f a -> m a)  
+interpret :: (Functor f, Monad m) 
+    => (f a -> m a)
+    -> (Free f a -> m a)  
 interpret phi = monad . freeM phi
